24555edc8437984fd17565882db2d3d944817b5c
kohei sasaki <sasaki_kohei@cyberagent.co.jp>
2015-11-12 21:35:30 +0900

    insert    POST  /movie   json
    update
        PUT   /movie/:id
    
    delete
        DELETE /movie/:id

--- src/main/java/com/meteora/ServerVerticle.java
+++ src/main/java/com/meteora/ServerVerticle.java
@@ -28,11 +28,11 @@ public class ServerVerticle extends AbstractVerticle {
     @Override
     public void start() throws Exception {
 
-        JsonObject config = new JsonObject().put("url", "jdbc:mysql://52.192.148.251:3306/meteora")
+        JsonObject config = new JsonObject().put("url", "jdbc:mysql://52.192.150.26:3306/meteora?characterEncoding=utf8")
                 .put("user","meteora-usr")
-                .put("max_pool_size", 100)
-                .put("initial_pool_size", 100)
-                .put("min_pool_size", 100);
+                .put("max_pool_size", 1)
+                .put("initial_pool_size", 1)
+                .put("min_pool_size", 1);
         DeploymentOptions options = new DeploymentOptions().setConfig(config).setInstances(2).setMultiThreaded(true).setWorker(true);
 
         vertx.deployVerticle("service:io.vertx.jdbc-service", options, res -> {
@@ -48,6 +48,9 @@ public class ServerVerticle extends AbstractVerticle {
 
                 router.get("/movie").handler(this::getListProduct);
                 router.get("/movie/:id").handler(this::getShowProduct);
+                router.post("/movie").handler(this::addProduct);
+                router.put("/movie/:id").handler(this::updateProduct);
+                router.delete("/movie/:id").handler(this::deleteProduct);
                 vertx.createHttpServer().requestHandler(router::accept).listen(8081);
             } else {
                 throw new RuntimeException(res.cause());
@@ -108,6 +111,68 @@ public class ServerVerticle extends AbstractVerticle {
                     } else {
                         context.response().end(rows.get(0).encode());
                     }
+                } else {
+                    context.fail(res.cause());
+                }
+            });
+        }
+    }
+
+    private void addProduct(RoutingContext context) {
+        JsonObject param = context.getBodyAsJson();
+        System.out.println(param);
+
+        if (param == null) {
+            context.fail(404);
+        } else {
+            SqlConnection connection = context.get(CONNECTION);
+            String sql = "INSERT INTO `meteora`.`movie` ( `title`, `description`) VALUES ( ? ,  ? );";
+            JsonArray params = new JsonArray().add(param.getValue("title")).add(param.getValue("description"));
+
+            connection.updateWithParams(sql, params, res -> {
+                if (res.succeeded()) {
+                    context.response().end();
+                } else {
+                    context.fail(res.cause());
+                }
+            });
+        }
+    }
+
+    private void updateProduct(RoutingContext context) {
+        String id = context.request().getParam("id");
+        JsonObject param = context.getBodyAsJson();
+
+        if (param == null || id == null) {
+            context.fail(404);
+        } else {
+            SqlConnection connection = context.get(CONNECTION);
+            String sql = "UPDATE movie SET title = ? , description = ?  where id = ? ;";
+            JsonArray params = new JsonArray().add(param.getValue("title")).add(param.getValue("description")).add(id);
+
+            connection.updateWithParams(sql, params, res -> {
+                if (res.succeeded()) {
+                    context.response().end();
+                } else {
+                    context.fail(res.cause());
+                }
+            });
+        }
+    }
+
+    private void deleteProduct(RoutingContext context) {
+        String id = context.request().getParam("id");
+
+        if ( id == null) {
+            context.fail(404);
+        } else {
+            SqlConnection connection = context.get(CONNECTION);
+            String sql = "delete from movie where id = ? ";
+            JsonArray params = new JsonArray().add(id);
+
+            connection.updateWithParams(sql, params, res -> {
+                if (res.succeeded()) {
+                    context.response().end();
                 } else {
                     context.fail(res.cause());
                 }
